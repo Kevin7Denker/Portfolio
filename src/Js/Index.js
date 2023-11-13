6b3e395b1308b0a44e6f533e1c89269060873f75
Kevin Denker <oficialdenker@gmail.com>
2023-11-13 14:57:06 -0300

    styles

--- src/Js/Index.js
+++ src/Js/Index.js
@@ -1,8 +1,6 @@
-//TopButton
+// -------- TopButton -------- //
 
 let mybutton = document.getElementById("Top");
-
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -13,8 +11,7 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
